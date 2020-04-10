bc7a6d12af3f5668d7d4ea40d1249608923195d1
na-wu <nwu1018@gmail.com>
2020-04-10 13:53:45 -0700

    working endpoint

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require("express")
 const bodyparser = require('body-parser')
 const app = express()
 const env = require('./env.json')
+var QRCode = require('qrcode')
+
+
+
+async function generate(data) {
+    return new Promise(function(resolve, reject) {
+        QRCode.toFile('./qrcode.png', data, function(err, url) {
+            if (err) {
+                reject(err)
+            } else {
+                console.log(url)
+                resolve(url)
+            }
+        })
+    })
+
+}
 
 var port = process.env.PORT || 3000
 
@@ -16,15 +33,18 @@ app.get('/', function(req, res, next) {
     res.send("Welcome to QRapi")
 })
 
-app.get("/api/", function(req, res, next) {
+app.get("/api", function(req, res, next) {
     res.send("Welcome to API!")
 })
 
-app.post("/api/generate/", function(req, res, next) {
+app.post("/api/generate", async function(req, res, next) {
     console.log("generating qr code...")
-    res.send(req.body)
+    const data = req.body["url"]
+    const qr = await generate(data)
+    res.sendFile(__dirname + '/qrcode.png')
+
 })
 
-app.get("/api/generate/", function(req, res, next) {
+app.get("/api/generate", function(req, res, next) {
     res.send(req.body)
 })

ed634e9a346697c3dce09ca081f5e5ad71980fc9
na-wu <nwu1018@gmail.com>
2020-04-11 03:21:05 -0700

    generate uuid per image

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const bodyparser = require('body-parser')
 const app = express()
 const env = require('./env.json')
 var QRCode = require('qrcode')
+const { v4: uuidv4 } = require('uuid');
 
 
 
-async function generate(data) {
+
+async function generate(data, uuid) {
     return new Promise(function(resolve, reject) {
-        QRCode.toFile('./qrcode.png', data, function(err, url) {
+        QRCode.toFile('./public/' + uuid, data, function(err, url) {
             if (err) {
                 reject(err)
             } else {
@@ -24,13 +26,14 @@ var port = process.env.PORT || 3000
 
 app.use(bodyparser.json()) // JSON data
 app.use(bodyparser.urlencoded({ extended: false })) //UTF-8 data
+app.use(express.static(__dirname + 'public'));
 
 app.listen(port, function() {
     console.log("Server running on port", port)
 })
 
 app.get('/', function(req, res, next) {
-    res.send("Welcome to QRapi")
+    res.send("Welcome to QRapi - " + uuidv4())
 })
 
 app.get("/api", function(req, res, next) {
@@ -40,11 +43,16 @@ app.get("/api", function(req, res, next) {
 app.post("/api/generate", async function(req, res, next) {
     console.log("generating qr code...")
     const data = req.body["url"]
-    const qr = await generate(data)
-    res.sendFile(__dirname + '/qrcode.png')
+    const id = uuidv4().toString() + '.png'
+    const qr = await generate(data, id)
+    var result = {}
+    result['url'] = id
+    res.send(result)
 
 })
 
-app.get("/api/generate", function(req, res, next) {
-    res.send(req.body)
+app.get("/:uuid", function(req, res, next) {
+    var params = req.params
+    console.log(params)
+    res.sendFile(__dirname + '/public/' + params['uuid'])
 })
